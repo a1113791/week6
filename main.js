463283cce3636ed999b599e562788cffad3f3ef5
hua <a1113791@gmail.com>
2024-08-30 00:35:15 +0800

    修改輪播

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+var swiper = new Swiper(".swiper", {
+	// 創建一個輪播
+	autoplay: {
+		// 自動輪播 swiper
+		delay: 2 * 1000, // 每兩秒切換下一張
+	},
+	loop: true, // 輪播結束後回到第一張繼續輪播
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction",
+	},
+	navigation: {
+		// 前一張、下一張按鈕
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
