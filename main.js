dfa439496570d48171124a84f02352e614c7f7b9
hua <a1113791@gmail.com>
2024-09-06 14:15:47 +0800

    修改

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 var swiper = new Swiper(".swiper", {
-	// 創建一個輪播
-	autoplay: {
+    // 創建一個輪播
+    /*autoplay: {
 		// 自動輪播 swiper
 		delay: 2 * 1000, // 每兩秒切換下一張
-	},
-	loop: true, // 輪播結束後回到第一張繼續輪播
-	pagination: {
-		el: ".swiper-pagination",
-		type: "fraction",
-	},
-	navigation: {
-		// 前一張、下一張按鈕
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
+	},*/
+    loop: true, // 輪播結束後回到第一張繼續輪播
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        // 前一張、下一張按鈕
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
